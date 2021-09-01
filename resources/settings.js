b2f2eaa14c4469bd638c8c383e4969ee1ea9e0d6
TheRolf <yann.lv@live.fr>
2021-09-01 12:25:58 +0200

    Fixed not charging images for contributions

--- resources/settings.js
+++ resources/settings.js
@@ -2,8 +2,36 @@ const ADMIN_ROLE = ['Administrator']
 const DEV_ROLE = ['Developer']
 const TRANSLATOR_ROLE = ['Translator']
 
+// Default texture repo for java
+const DEFAULT_MC_JAVA_REPOSITORY    = 'https://raw.githubusercontent.com/CompliBot/Default-Java/'
+const DEFAULT_MC_BEDROCK_REPOSITORY = 'https://raw.githubusercontent.com/CompliBot/Default-Bedrock/'
+
+const COMPLIANCE_32X_JAVA_REPOSITORY_JAPPA = 'https://raw.githubusercontent.com/Compliance-Resource-Pack/Compliance-Java-32x/Jappa-'
+const COMPLIANCE_64X_JAVA_REPOSITORY_JAPPA = 'https://raw.githubusercontent.com/Compliance-Resource-Pack/Compliance-Java-64x/Jappa-'
+
+const COMPLIANCE_32X_BEDROCK_REPOSITORY_JAPPA = 'https://raw.githubusercontent.com/Compliance-Resource-Pack/Compliance-Bedrock-32x/Jappa-'
+const COMPLIANCE_64X_BEDROCK_REPOSITORY_JAPPA = 'https://raw.githubusercontent.com/Compliance-Resource-Pack/Compliance-Bedrock-64x/Jappa-'
+
+const LATEST_MC_JE_VERSION = '1.17'
+const LATEST_MC_BE_VERSION = '1.17.0'
+
 module.exports = {
   ADMIN_ROLE,
   DEV_ROLE,
-  TRANSLATOR_ROLE
+  TRANSLATOR_ROLE,
+
+  DEFAULT_MC_JAVA_REPOSITORY,
+	DEFAULT_MC_BEDROCK_REPOSITORY,
+
+	COMPLIANCE_32X_JAVA_REPOSITORY_JAPPA,
+	COMPLIANCE_64X_JAVA_REPOSITORY_JAPPA,
+	COMPLIANCE_32X_BEDROCK_REPOSITORY_JAPPA,
+	COMPLIANCE_64X_BEDROCK_REPOSITORY_JAPPA,
+
+	COMPLIANCE_32X_BEDROCK_REPOSITORY_JAPPA,
+	COMPLIANCE_64X_BEDROCK_REPOSITORY_JAPPA,
+
+	LATEST_MC_JE_VERSION,
+	LATEST_MC_BE_VERSION,
+  
 }
