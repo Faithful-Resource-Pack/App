Faithful-Resource-Pack/App

commit 730c07fa4600f65319b51fce24dc335b42d16bc2

use 2x2 checkerboard for gallery, closes #15

pages/gallery/modal.js
@@ -39,7 +39,7 @@ export default {
                 <div class="gallery-dialog-texture-container">
                   <div
                     class="gallery-dialog-texture"
-                    style="background: url(https://raw.githubusercontent.com/Compliance-Resource-Pack/Website/master/image/background/transparency_16x.png)"
+                    style="background: url(https://raw.githubusercontent.com/Compliance-Resource-Pack/App/main/resources/transparency.png)"
                   >
                     <img class="gallery-texture-image" onerror="this.style.display='none'; this.nextElementSibling.style.display='block'; this.parentElement.style.background='rgba(0,0,0,0.3)';this.parentElement.classList.add('rounded')" :src="getTextureURL(res)" lazy-src="https://database.compliancepack.net/images/bot/loading.gif" />
                     <div class="not-done" style="display: none;">

pages/gallery/gallery.js
@@ -61,7 +61,7 @@ export default {
           class="gallery-texture-in-container"
           v-tooltip.right-start="{content: () => getAuthor(texture.textureID), html: true}"
           v-on:click="openModal(texture.textureID)"
-          style="background: url(https://raw.githubusercontent.com/Compliance-Resource-Pack/Website/master/image/background/transparency_16x.png)"
+          style="background: url(https://raw.githubusercontent.com/Compliance-Resource-Pack/App/main/resources/transparency.png)"
         >
           <img class="gallery-texture-image" onerror="this.style.display='none'; this.nextElementSibling.style.display='block'; this.parentElement.style.background='rgba(0,0,0,0.3)';this.parentElement.classList.add('rounded')" :src="getTextureURL(texture.useID)" lazy-src="https://database.compliancepack.net/images/bot/loading.gif" />
           <div class="not-done" style="display: none;">

commit be147cbd55d8ce27d00a3d06a95ef9b06e9cea0e

fix background image issue in texture modal introduced in 730c07f

pages/gallery/modal.js
@@ -39,7 +39,7 @@ export default {
                 <div class="gallery-dialog-texture-container">
                   <div
                     class="gallery-dialog-texture"
-                    style="background: url(https://raw.githubusercontent.com/Compliance-Resource-Pack/App/main/resources/transparency.png)"
+                    style="background: url(https://raw.githubusercontent.com/Compliance-Resource-Pack/App/main/resources/transparency.png) center"
                   >
                     <img class="gallery-texture-image" onerror="this.style.display='none'; this.nextElementSibling.style.display='block'; this.parentElement.style.background='rgba(0,0,0,0.3)';this.parentElement.classList.add('rounded')" :src="getTextureURL(res)" lazy-src="https://database.compliancepack.net/images/bot/loading.gif" />
                     <div class="not-done" style="display: none;">