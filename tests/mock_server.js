7a76c67cee5c65d670cd6e786b8904fadad85e41
Evorp <3vorpgaming@gmail.com>
2024-02-22 22:33:51 -0800

    remove unused dependencies and discord auth

--- tests/mock_server.js
+++ tests/mock_server.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var cors = require("cors");
+const express = require("express");
+const cors = require("cors");
 
 require("dotenv").config({
 	path: __dirname + "/.env.test",
